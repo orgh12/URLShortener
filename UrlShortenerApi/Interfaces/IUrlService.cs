4a0ed642d357c96d322e295341a4a0d38585c8f0
orgh12 <or.shenbahar@gmail.com>
2025-06-11 22:59:41 +0300

    Fixed some build warnings - added limit to urlMapping

--- UrlShortenerApi/Interfaces/IUrlService.cs
+++ UrlShortenerApi/Interfaces/IUrlService.cs
@@ -3,6 +3,6 @@ namespace UrlShortenerApi.Interfaces;
 public interface IUrlService
 {
     public string ShortenUrl(string originalUrl);
-    public string GetOriginalUrl(string shortCode);
+    public string? GetOriginalUrl(string shortCode);
     public string GenerateCode(string shortCode);
 }
--- UrlShortenerApi/Models/UrlMapping.cs
+++ UrlShortenerApi/Models/UrlMapping.cs
@@ -5,8 +5,10 @@ namespace UrlShortenerApi.Models;
 public class UrlMapping
 {
     public int Id { get; set; }
-    public string ShortCode { get; set; }
-    public string OriginalUrl { get; set; }
+    [MaxLength(8)]
+    public required string ShortCode { get; set; }
+    [MaxLength(2000)]
+    public required string OriginalUrl { get; set; }
     public int ClickedOn { get; set; } = 0;
     public DateTime CreatedOn { get; set; } = DateTime.Now;
 }
