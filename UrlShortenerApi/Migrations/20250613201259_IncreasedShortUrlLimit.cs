a77238f866389618e52af246bb53f44a7a829853
orgh12 <or.shenbahar@gmail.com>
2025-06-14 11:24:09 +0300

    Forgot to commit

---
+++ UrlShortenerApi/Migrations/20250613201259_IncreasedShortUrlLimit.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortenerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasedShortUrlLimit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortCode",
+                table: "UrlMappings",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortCode",
+                table: "UrlMappings",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
